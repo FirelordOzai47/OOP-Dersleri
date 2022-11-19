8fd87315ced0b00c77134db97b2af8c00a0d8f7c
FirelordOzai47 <mustafadgn.4@gmail.com>
2022-11-19 23:58:59 +0300

    .

---
+++ DöngüDenemeleri/Program.cs
@@ -0,0 +1,17 @@
+﻿//Girilen 10 sayı içinde pozitif ve tek olan sayıların çarpımını yazdıran
+int sayi;
+int sonuc = 1;
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine("{0}.Sayıyı Giriniz;", i);
+    sayi = Convert.ToInt32(Console.ReadLine());
+    if ((sayi > 0) && (sayi % 2 == 1))
+    {
+        sonuc *= sayi;
+    }
+    
+}
+
+Console.WriteLine("Pozitif ve tek sayıların çarpımı : " + sonuc);
+Console.ReadKey();
+    
